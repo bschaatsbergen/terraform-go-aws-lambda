9d09d4dbf4324e085e221dcf5eb78ede3659c49a
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-05-05 21:03:46 +0200

    Initial commit

---
+++ example/my-app/go.mod
@@ -0,0 +1,5 @@
+module my-app
+
+go 1.22.2
+
+require github.com/aws/aws-lambda-go v1.47.0
---
+++ example/my-app/lambda.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/aws/aws-lambda-go/lambda"
+
+func handleRequest() (string, error) {
+	return "Hello from Go!", nil
+}
+func main() {
+	lambda.Start(handleRequest)
+}
